c7e24f13c25443b3eddb82e7705a9e34ab7df6c4
iamShimanto <shimantowebdeveloper@gmail.com>
2025-09-27 02:38:54 +0600

    square patterns

--- squarePatterns.cpp
+++ squarePatterns.cpp
@@ -13,5 +13,30 @@ int main() {
     cout << endl;
   }
 
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+
+  for (int i = 0; i < n; i++) {
+    char ch = 'A';
+    for (int j = 0; j < n; j++) {
+      cout << ch;
+      ch = ch + 1;
+    }
+    cout << endl;
+  }
+
+  int num = 1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout << num << " ";
+      num = num + 1;
+    }
+    cout << endl;
+  }
+
   return 0;
 }
